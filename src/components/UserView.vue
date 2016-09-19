9a9803a6d79a73f7ca08f0c8fe2f0855abc61558
Slava <horlet@gmail.com>
2016-09-19 14:23:24 +0300

    (chore) router - added filter and user views, just templates

---
+++ src/components/UserView.vue
@@ -0,0 +1,69 @@
+<template>
+    <div>
+        <h1>О пользователе</h1>
+        <!--<input v-model="newTodo" v-on:keyup.enter="addTodo">-->
+        <!--<ul>-->
+            <!--<li v-for="todo in todos">-->
+                <!--<span>{{ todo.text }}</span>-->
+                <!--<button v-on:click="removeTodo($index)">X</button>-->
+            <!--</li>-->
+        <!--</ul>-->
+    </div>
+</template>
+
+<script>
+    //import store from '../store'
+
+    export default {
+
+        name: 'UserView',
+
+        data: () => ({
+            newTodo: '',
+            todos: [
+                {text: 'Пример текста задачи'}
+            ]
+        }),
+        created: function() {
+            this.loadTodos()
+        },
+        methods: {
+            addTodo: function () {
+                let text = this.newTodo.trim()
+                let unique = true
+                if (text) {
+                    // Check for uniqueness
+                    this.todos.forEach((el)=>{
+                        if(el.text == text) {
+                            unique = false
+                        }
+                    })
+                    if (unique) {
+                        this.todos.push({text: text})
+                        this.storeTodos()
+                    }
+                    this.newTodo = ''
+                }
+            },
+            removeTodo: function (index) {
+                this.todos.splice(index, 1)
+                this.storeTodos()
+            },
+            storeTodos: function () {
+                let data = JSON.stringify(this.todos)
+                window.localStorage.setItem('todos', data)
+            },
+            loadTodos: function () {
+                let data = window.localStorage.getItem('todos')
+                if (data) {
+                    data = JSON.parse(data)
+                    this.todos = data
+                }
+            }
+        }
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
